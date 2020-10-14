46d9d10063224c1f220a3e581417109b3da81f86
Luccas <luccas@comp.ufla.br>
2020-10-14 17:50:59 -0300

    Removed unused variables

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import Leaflet from 'leaflet'
 
 import mapMarkerImg from '../images/map-marker.svg'
 import mapIcon from '../utils/mapIcon'
